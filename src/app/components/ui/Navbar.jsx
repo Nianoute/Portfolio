4519e1f49c822d0812df5b1b1a53d7adc68a6e83
nianoute <enzo.angot@gmail.com>
2023-11-07 16:52:23 +0100

    first commit

---
+++ src/app/components/ui/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavbarMain = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">Angito</div>
+    </div>
+  );
+};
+
+export default NavbarMain;
